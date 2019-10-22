674cd9333e8dbd125fdabde286e6ab927beeb701
Simon <simon.robl@live.de>
2019-10-22 18:02:40 +0200

    Add some basic turret stubs

---
+++ DotsTowerDefense/Assets/Scripts/Turrets/Components.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct MachineGun : IComponentData{}
+
+public struct LazerBeam : IComponentData {}
+
+public struct FreezeGun : IComponentData {}
+
+public struct Frozen : IComponentData {}
+
+public struct FireTimer: IComponentData
+{
+    public float cooldown;
+    public float countdown;
+}
+
+public struct FireBullet: IComponentData {}
+
+public struct FireTarget : IComponentData
+{
+    public float3 position;
+}
+
+public struct RelativeBulletSpawnPosition : IComponentData
+{
+    public float3 value;
+}
---
+++ DotsTowerDefense/Assets/Scripts/Turrets/Systems.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class FireTimerSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Entity entity, ref FireTimer timer) =>
+        {
+            timer.countdown -= UnityEngine.Time.deltaTime;
+            if(timer.cooldown <= 0f)
+            {
+                EntityManager.AddComponent<FireBullet>(entity);
+            }
+        });
+    }
+}
+
+public class FireBulletSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities
+            .With(GetEntityQuery(typeof(FireBullet)))
+            .ForEach((ref RelativeBulletSpawnPosition spawnPos) =>
+            {
+                var bullet = EntityManager.CreateEntity();
+                EntityManager.AddComponentData(bullet, 
+                    new Translation { Value = spawnPos.value });
+
+            });
+    }
+}
